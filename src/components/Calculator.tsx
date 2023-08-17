77fe91a6f92f08b02b9e52090028dd9a1b472dd2
GlowyBlack <a.oludare1234@gmail.com>
2023-08-17 07:44:53 -0600

    Finished Calculator(?)

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -15,9 +15,57 @@ function getAgePoints(age: number): number {
   if (age === 44) return 3;
   if (age === 45) return 2;
   if (age === 46) return 1;
-  // ... add other conditions for age here
   return 0;
 }
+
+function getWorkExperiencePoints(type: PrincipalCanadaWork): number {
+  if (type === "Working in Canada" || type === "LMIA Job Offer") return 10;
+  return 0;
+}
+
+function getWorkYearsPoints(years: number): number {
+  if (years === 1) return 9;
+  if (years === 2 || years === 3) return 11;
+  if (years === 4 || years === 5) return 13;
+  if (years > 5) return 15;
+  return 0;
+}
+
+function getAdaptabilityPoints(
+  principalCanadaWork: PrincipalCanadaWork,
+  principalPreviousStudy: YesNo,
+  spousePreviousStudy: YesNo,
+  spouseCanadaWork: YesNo,
+  spouseLanguage: YesNo,
+  canadianRelative: YesNo
+): number {
+  let points = 0;
+
+  // Principal Canada Work Points
+  if (principalCanadaWork === "Working in Canada") points += 10;
+
+  // Principal Previous Study Points
+  if (principalPreviousStudy === "Yes") points += 5;
+
+  // LMIA Job Offer Points
+  if (principalCanadaWork === "LMIA Job Offer") points += 5;
+
+  // Spouse Previous Study Points
+  if (spousePreviousStudy === "Yes") points += 5;
+
+  // Spouse Canada Work Points
+  if (spouseCanadaWork === "Yes") points += 5;
+
+  // Spouse Language Points
+  if (spouseLanguage === "Yes") points += 5;
+
+  // Canadian Relative Points
+  if (canadianRelative === "Yes") points += 5;
+
+  // Return the minimum between the calculated points and 10 (as per the provided formula)
+  return Math.min(10, points);
+}
+
 
 const Calculator: React.FC = () => {
   const [englishScore, setEnglishScore] = useState<number>(0);
